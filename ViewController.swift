99ca8e827259bb0892ef9ff0391fd9ee91981c4c
JoeboJiang <jiubai.jiang@kodak.com>
2017-02-08 09:59:48 +0800

    Remove bad

--- ViewController.swift
+++ ViewController.swift
@@ -11,24 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        //sunny3
-        //joebo
-        //hello world
-        //fix bugs
-        //
-        //modify at master branch
-        //test1
-        //fix a bug
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-        //1
-        //2
-        //3
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        ////ddd
-        // Dispose of any resources that can be recreated.
     }
 }
 
