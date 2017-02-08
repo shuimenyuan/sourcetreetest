166b1562cedb9c4a077fbf05e8b1b39b840dfbba
JoeboJiang <jiubai.jiang@kodak.com>
2017-02-08 09:57:34 +0800

    init

---
+++ ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Joebo Jiang on 18/01/2017.
+//  Copyright © 2017 huan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //sunny3
+        //joebo
+        //hello world
+        //fix bugs
+        //
+        //modify at master branch
+        //test1
+        //fix a bug
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+        //1
+        //2
+        //3
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        ////ddd
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
