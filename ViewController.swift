03db344f3f80b2de8a9f2aac61dc128250f7bea0
JoeboJiang <jiubai.jiang@kodak.com>
2017-02-09 09:25:46 +0800

    Revert "1"
    This reverts commit c28549fd1add1ffbe36b85d0e474c8cf7eb5f616.

--- ViewController.swift
+++ ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        //1
         }
 
     override func didReceiveMemoryWarning() {
