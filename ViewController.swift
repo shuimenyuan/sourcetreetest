367c7392ffa7a798a393cc2beef4a8fa4856f053
JoeboJiang <jiubai.jiang@kodak.com>
2017-02-09 09:22:52 +0800

    人

--- ViewController.swift
+++ ViewController.swift
@@ -11,13 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // a11
         }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        //esf
     }
 }
-//e
 
